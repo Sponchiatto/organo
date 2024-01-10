325b0ba4ecb0934313d1b875a0b7401cc34534a8
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-01-10 15:07:02 -0300

    Banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" alt="O Banner da página Organa"></img>
+    </header>
+  );
+}
+
+export default Banner;
